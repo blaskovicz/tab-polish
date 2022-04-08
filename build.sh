9a73e66737324144b830e9d8327ce65b29638ec4
Zach Auclair <zauclair@salesforce.com>
2022-04-08 15:31:47 -0400

    chore(build): support macos

--- build.sh
+++ build.sh
@@ -2,12 +2,18 @@
 
 set -euxo pipefail
 
+SED=sed
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Mac OSX
+  SED=gsed
+fi
+
 archive=tab-polish.zip
 version=$(git describe --tags | sed 's/^v//' | cut -d'-' -f1,1)
 react-scripts build
 cp *.png ./build
 cp manifest.json ./build
-sed -i 's/@@VERSION@@/'$version'/g' ./build/manifest.json
+$SED -i 's/@@VERSION@@/'$version'/g' ./build/manifest.json
 find ./build -type f
 (cd build && zip -r9 - .) > $archive
 echo "$archive v$version created."
