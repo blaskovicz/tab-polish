8573baa63253991f4b489439afe64c1d7182d3f3
Zach Auclair <zach@Zachs-MacBook-Pro.local>
2025-02-18 00:09:53 -0500

    chore(build): macos fixes, node 18

--- build.sh
+++ build.sh
@@ -2,18 +2,11 @@
 
 set -euxo pipefail
 
-SED=sed
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  # Mac OSX
-  SED=gsed
-fi
-
 archive=tab-polish.zip
 version=$(git describe --tags | sed 's/^v//' | cut -d'-' -f1,1)
 react-scripts build
 cp *.png ./build
-cp manifest.json ./build
-$SED -i 's/@@VERSION@@/'$version'/g' ./build/manifest.json
+sed 's/@@VERSION@@/'$version'/g' ./manifest.json > ./build/manifest.json
 find ./build -type f
 (cd build && zip -r9 - .) > $archive
 echo "$archive v$version created."
